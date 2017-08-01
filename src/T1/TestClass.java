81b0eb056067def4366caa5bf675f1592a804906
ogrinenko <ogrinenko@HP-073.intropro.com.ua>
2017-08-01 11:51:11 +0300

    Added Constructor

--- src/T1/TestClass.java
+++ src/T1/TestClass.java
@@ -1,10 +1,19 @@
 package T1;
 
 public class TestClass {
+	
+	
+	public TestClass()
+	{
+		System.out.println("Test Class Constructor");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		TestClass tC = new TestClass();
+		
 	}
 
 }
