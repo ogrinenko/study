5943185aced03682bda45d59b37cbdd019286cb5
dtv <dtv@192.168.1.71>
2017-08-03 11:12:20 -0700

    exceptions and collections

---
+++ src/collections/TestCollections.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.ArrayList;
+
+
+
+public class TestCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> myArray = new ArrayList<String>(); 
+		
+		myArray.add(5);
+		
+		
+		
+		
+	}
+
+}
---
+++ src/exceptions/TestExceptions.java
@@ -0,0 +1,43 @@
+package exceptions;
+
+import java.io.IOException;
+
+public class TestExceptions {
+
+	public static void divideTwoNumbers(int i, int m)
+	{
+		System.out.println(i/m);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+		if (i > 0)
+		{
+			System.out.println("000");
+			throw new ArithmeticException("Lolka");
+		}
+		
+		try {
+			divideTwoNumbers(0, 0);
+		} catch (ArithmeticException e) {
+			// TODO: handle exception
+			System.out.println("111" + e.getMessage());
+			
+		}
+		catch (Exception e) {
+		// TODO: handle exception
+		System.out.println("222" + e.getMessage());
+		}
+				
+		finally {
+			
+			
+			
+		}
+		
+	}
+
+}
