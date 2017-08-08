b21bbe66bca704ad2014837c4f17697f98dceeb9
ogrinenko <ogrinenko@HP-073.intropro.com.ua>
2017-08-08 13:43:19 +0300

    students overload

--- src/T5/Students.java
+++ src/T5/Students.java
@@ -17,5 +17,29 @@ public class Students extends University{
 		studentCourse = sCourse;
 	}
 	
-
+	@Override
+	public int hashCode() {
+	  int result = 1;
+	  result = result + ((studentLastName == null) ? 0 : studentLastName.hashCode());
+	  return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	    if (this == obj)
+	      return true;
+	    if (obj == null)
+	      return false;
+	    if (getClass() != obj.getClass())
+	      return false;
+	    Students other = (Students) obj;
+	    if ((studentLastName == null) || (studentFirstName == null)) {
+	      if ((other.studentLastName != null) || (other.studentFirstName != null))
+	        return false;
+	    } else if ((!studentLastName.equals(other.studentLastName)) || (!studentFirstName.equals(other.studentFirstName))) 
+	      return false;
+	    return true;
+	  }
+	
+	
 }
